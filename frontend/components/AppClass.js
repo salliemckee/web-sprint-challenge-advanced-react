7e6bf664c53adb85a156a682c09c831a25730e6a
Sallie McKee <salliemckee@Morrigan.local>
2022-04-02 16:44:07 -0500

    finished project

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,13 +1,192 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
 
 export default class AppClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      position: { x: 2, y: 2 },
+      timesMoved: 0,
+      message: "",
+      email: "",
+      s: "s",
+    };
+
+    this.handleEmail = this.handleEmail.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.removeStyle = this.removeStyle.bind(this);
+    this.buttonClick = this.buttonClick.bind(this);
+  }
+
+  handleEmail(e) {
+    this.setState({ ...this.state, email: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.email.trim() === "") {
+      this.setState({ ...this.state, message: "Ouch: email is required" });
+    } else if (this.state.email === "foo@bar.baz") {
+      this.setState({ ...this.state, message: "foo@bar.baz failure #71" });
+    } else {
+      axios
+        .post("http://localhost:9000/api/result", {
+          x: this.state.position.x,
+          y: this.state.position.y,
+          steps: this.state.timesMoved,
+          email: this.state.email,
+        })
+        .then((res) => {
+          this.setState({
+            ...this.state,
+            message: res.data.message,
+            email: "",
+          });
+        })
+        .catch((err) => {
+          this.setState({
+            ...this.state,
+            message: "Ouch: email must be a valid email",
+          });
+          console.log(err);
+        });
+    }
+  }
+
+  grid = [
+    { x: 1, y: 1 },
+    { x: 2, y: 1 },
+    { x: 3, y: 1 },
+    { x: 1, y: 2 },
+    { x: 2, y: 2 },
+    { x: 3, y: 2 },
+    { x: 1, y: 3 },
+    { x: 2, y: 3 },
+    { x: 3, y: 3 },
+  ];
+
+  removeStyle() {
+    this.grid.forEach((pos, index) => {
+      if (JSON.stringify(pos) === JSON.stringify(this.state.position)) {
+        const element = document.querySelector(
+          `#grid :nth-child(${index + 1})`
+        );
+        element.textContent = "";
+        element.classList.remove("active");
+      }
+    });
+  }
+
+  componentDidUpdate() {
+    this.grid.forEach((pos, index) => {
+      if (JSON.stringify(pos) === JSON.stringify(this.state.position)) {
+        const element = document.querySelector(
+          `#grid :nth-child(${index + 1})`
+        );
+        element.textContent = "B";
+        element.classList.add("active");
+      }
+    });
+  }
+
+  buttonClick(e) {
+    switch (e.target.id) {
+      case "up":
+        if (this.state.position.y === 1) {
+          this.setState({ ...this.state, message: "You can't go up" });
+        } else {
+          this.removeStyle();
+          this.setState({
+            ...this.state,
+            position: {
+              x: this.state.position.x,
+              y: this.state.position.y - 1,
+            },
+            message: "",
+            timesMoved: this.state.timesMoved + 1,
+            s: this.state.timesMoved + 1 === 1 ? "" : "s",
+          });
+        }
+        break;
+      case "down":
+        if (this.state.position.y === 3) {
+          this.setState({ ...this.state, message: "You can't go down" });
+        } else {
+          this.removeStyle();
+          this.setState({
+            ...this.state,
+            position: {
+              x: this.state.position.x,
+              y: this.state.position.y + 1,
+            },
+            message: "",
+            timesMoved: this.state.timesMoved + 1,
+            s: this.state.timesMoved + 1 === 1 ? "" : "s",
+          });
+        }
+        break;
+      case "left":
+        if (this.state.position.x === 1) {
+          this.setState({ ...this.state, message: "You can't go left" });
+        } else {
+          this.removeStyle();
+          this.setState({
+            ...this.state,
+            position: {
+              x: this.state.position.x - 1,
+              y: this.state.position.y,
+            },
+            message: "",
+            timesMoved: this.state.timesMoved + 1,
+            s: this.state.timesMoved + 1 === 1 ? "" : "s",
+          });
+        }
+        break;
+      case "right":
+        if (this.state.position.x === 3) {
+          this.setState({ ...this.state, message: "You can't go right" });
+        } else {
+          this.removeStyle();
+          this.setState({
+            ...this.state,
+            position: {
+              x: this.state.position.x + 1,
+              y: this.state.position.y,
+            },
+            message: "",
+            timesMoved: this.state.timesMoved + 1,
+            s: this.state.timesMoved + 1 === 1 ? "" : "s",
+          });
+        }
+        break;
+      case "reset":
+        this.removeStyle();
+        this.setState({
+          ...this.state,
+          position: {
+            x: 2,
+            y: 2,
+          },
+          message: "",
+          timesMoved: 0,
+          email: "",
+          s: "s",
+        });
+        break;
+    }
+  }
+
   render() {
-    const { className } = this.props
+    const { className } = this.props;
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">
+            Coordinates ({this.state.position.x}, {this.state.position.y})
+          </h3>
+          <h3 id="steps">
+            You moved {this.state.timesMoved} time{this.state.s}
+          </h3>
         </div>
         <div id="grid">
           <div className="square"></div>
@@ -21,20 +200,36 @@ export default class AppClass extends React.Component {
           <div className="square"></div>
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button id="left" onClick={this.buttonClick}>
+            LEFT
+          </button>
+          <button id="up" onClick={this.buttonClick}>
+            UP
+          </button>
+          <button id="right" onClick={this.buttonClick}>
+            RIGHT
+          </button>
+          <button id="down" onClick={this.buttonClick}>
+            DOWN
+          </button>
+          <button id="reset" onClick={this.buttonClick}>
+            reset
+          </button>
         </div>
         <form>
-          <input id="email" type="email" placeholder="type email"></input>
-          <input id="submit" type="submit"></input>
+          <input
+            id="email"
+            type="email"
+            placeholder="type email"
+            value={this.state.email}
+            onChange={this.handleEmail}
+          ></input>
+          <input id="submit" type="submit" onClick={this.handleSubmit}></input>
         </form>
       </div>
-    )
+    );
   }
 }
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,11 +1,139 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function AppFunctional(props) {
+  const [position, setPosition] = useState({ x: 2, y: 2 });
+  const [timesMoved, setTimesMoved] = useState(0);
+  const [message, setMessage] = useState("");
+  const [email, setEmail] = useState("");
+  const s = timesMoved !== 1 ? "s" : "";
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email.trim() === "") {
+      setMessage("Ouch: email is required");
+    } else if (email === "foo@bar.baz") {
+      setMessage("foo@bar.baz failure #71");
+    } else {
+      axios
+        .post("http://localhost:9000/api/result", {
+          x: position.x,
+          y: position.y,
+          steps: timesMoved,
+          email: email,
+        })
+        .then((res) => {
+          setMessage(res.data.message);
+          setEmail("");
+        })
+        .catch((err) => {
+          setMessage("Ouch: email must be a valid email");
+          console.log(err);
+        });
+    }
+  };
+
+  const grid = [
+    { x: 1, y: 1 },
+    { x: 2, y: 1 },
+    { x: 3, y: 1 },
+    { x: 1, y: 2 },
+    { x: 2, y: 2 },
+    { x: 3, y: 2 },
+    { x: 1, y: 3 },
+    { x: 2, y: 3 },
+    { x: 3, y: 3 },
+  ];
+
+  const removeStyle = () => {
+    grid.forEach((pos, index) => {
+      if (JSON.stringify(pos) === JSON.stringify(position)) {
+        const element = document.querySelector(
+          `#grid :nth-child(${index + 1})`
+        );
+        element.textContent = "";
+        element.classList.remove("active");
+      }
+    });
+  };
+
+  useEffect(() => {
+    grid.forEach((pos, index) => {
+      if (JSON.stringify(pos) === JSON.stringify(position)) {
+        const element = document.querySelector(
+          `#grid :nth-child(${index + 1})`
+        );
+        element.textContent = "B";
+        element.classList.add("active");
+      }
+    });
+  }, [position]);
+
+  const buttonClick = (e) => {
+    switch (e.target.id) {
+      case "up":
+        if (position.y === 1) {
+          setMessage("You can't go up");
+        } else {
+          removeStyle();
+          setMessage("");
+          setPosition({ x: position.x, y: position.y - 1 });
+          setTimesMoved(timesMoved + 1);
+        }
+        break;
+      case "down":
+        if (position.y === 3) {
+          setMessage("You can't go down");
+        } else {
+          removeStyle();
+          setMessage("");
+          setPosition({ x: position.x, y: position.y + 1 });
+          setTimesMoved(timesMoved + 1);
+        }
+        break;
+      case "left":
+        if (position.x === 1) {
+          setMessage("You can't go left");
+        } else {
+          removeStyle();
+          setMessage("");
+          setPosition({ x: position.x - 1, y: position.y });
+          setTimesMoved(timesMoved + 1);
+        }
+        break;
+      case "right":
+        if (position.x === 3) {
+          setMessage("You can't go right");
+        } else {
+          removeStyle();
+          setMessage("");
+          setPosition({ x: position.x + 1, y: position.y });
+          setTimesMoved(timesMoved + 1);
+        }
+        break;
+      case "reset":
+        removeStyle();
+        setMessage("");
+        setTimesMoved(0);
+        setPosition({ x: 2, y: 2 });
+        setEmail("");
+        break;
+    }
+  };
+
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">
+          Coordinates ({position.x}, {position.y})
+        </h3>
+        <h3 id="steps">
+          You moved {timesMoved} time{s}
+        </h3>
       </div>
       <div id="grid">
         <div className="square"></div>
@@ -19,19 +147,35 @@ export default function AppFunctional(props) {
         <div className="square"></div>
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button id="left" onClick={buttonClick}>
+          LEFT
+        </button>
+        <button id="up" onClick={buttonClick}>
+          UP
+        </button>
+        <button id="right" onClick={buttonClick}>
+          RIGHT
+        </button>
+        <button id="down" onClick={buttonClick}>
+          DOWN
+        </button>
+        <button id="reset" onClick={buttonClick}>
+          reset
+        </button>
       </div>
       <form>
-        <input id="email" type="email" placeholder="type email"></input>
-        <input id="submit" type="submit"></input>
+        <input
+          id="email"
+          type="email"
+          value={email}
+          onChange={handleEmail}
+          placeholder="type email"
+        ></input>
+        <input id="submit" type="submit" onClick={handleSubmit}></input>
       </form>
     </div>
-  )
+  );
 }
